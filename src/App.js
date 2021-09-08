57e6086781e6451be77da13dc8e7d722791edff2
ASV185 <74805696+ASV185@users.noreply.github.com>
2021-09-07 21:36:32 -0600

    added bootstrap to html and container

--- src/App.js
+++ src/App.js
@@ -9,12 +9,7 @@ function App() {
   return (
     <div className="Container">
       <div className="App">
-        <link
-          rel="stylesheet"
-          href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-          integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
-          crossorigin="anonymous"
-        />
+        
         <footer>
           <a href="https://github.com/ASV185/my-weather-app-project">
             Open-source code {""}
