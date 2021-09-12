0ac9f584251ee0ee5469e1bbb632694ee1ddde7a
ASV185 <74805696+ASV185@users.noreply.github.com>
2021-09-12 13:57:51 -0600

    remove some information for loader

--- src/Weather.js
+++ src/Weather.js
@@ -66,7 +66,7 @@ export default function Weather(props){
     return (
         <div className={"row"}>
             <div className={"item"}>
-                <Loader bgColor={"#011B10"} title={"bubble-scale"} color={'#011B10'} size={100} />
+                <Loader color={'#011B10'} size={100} />
             </div>
         </div>
     );
